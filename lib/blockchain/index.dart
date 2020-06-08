eddd16c93963ec1103e22bf8a244cce0fe858c46
Jør∂¡ <itsbrickpop@gmail.com>
2020-06-08 18:47:46 +0200

    Throwing an error when a domain does not exist

--- lib/blockchain/index.dart
+++ lib/blockchain/index.dart
@@ -7,33 +7,37 @@ import '../blockchain/ens.dart';
 final entityResolverDomain = "entity-resolver.vocdoni.eth";
 final votingProcessDomain = "voting-process.vocdoni.eth";
 
-String resolvedEntityResolverAddress;
-String resolvedVotingProcessAddress;
+String entityResolverAddress;
+String votingProcessAddress;
 
 /// Call a method on the given contract instance using the given Gateway URI
 /// and using the given parameters
 Future<List<dynamic>> callEntityResolverMethod(
     String gatewayUri, String method, List<dynamic> params) async {
-  Web3Gateway gw = Web3Gateway(gatewayUri);
-  if (resolvedEntityResolverAddress == null) {
-    resolvedEntityResolverAddress =
-        await resolveName(entityResolverDomain, gatewayUri);
+  final gw = Web3Gateway(gatewayUri);
+  if (entityResolverAddress == null) {
+    entityResolverAddress = await resolveName(entityResolverDomain, gatewayUri);
+
+    if (!(entityResolverAddress is String))
+      throw Exception("The domain name does not exist");
   }
 
-  return gw.callMethod(jsonEncode(entityResolverAbi),
-      resolvedEntityResolverAddress, method, params);
+  return gw.callMethod(
+      jsonEncode(entityResolverAbi), entityResolverAddress, method, params);
 }
 
 /// Call a method on the given contract instance using the given Gateway URI
 /// and using the given parameters
 Future<List<dynamic>> callVotingProcessMethod(
     String gatewayUri, String method, List<dynamic> params) async {
-  Web3Gateway gw = Web3Gateway(gatewayUri);
-  if (resolvedVotingProcessAddress == null) {
-    resolvedVotingProcessAddress =
-        await resolveName(votingProcessDomain, gatewayUri);
+  final gw = Web3Gateway(gatewayUri);
+  if (votingProcessAddress == null) {
+    votingProcessAddress = await resolveName(votingProcessDomain, gatewayUri);
+
+    if (!(votingProcessAddress is String))
+      throw Exception("The domain name does not exist");
   }
 
-  return gw.callMethod(jsonEncode(votingProcessAbi),
-      resolvedVotingProcessAddress, method, params);
+  return gw.callMethod(
+      jsonEncode(votingProcessAbi), votingProcessAddress, method, params);
 }
